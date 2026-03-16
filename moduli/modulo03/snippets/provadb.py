99f3b20c80ffe73243c55ffa4fcbba15719f4710
antoniochiurla <antoniochiurla@gmail.com>
2026-03-16 17:00:54 +0000

    fixed db container name on test script

---
+++ moduli/modulo03/snippets/provadb.py
@@ -0,0 +1,16 @@
+import psycopg2
+import os
+
+print("Hello World from Python running inside a Docker container!")
+
+try:
+    conn = psycopg2.connect(
+        host="database_dev",
+        database="devdb",
+        user="devuser",
+        password="devpassword")
+    print("Successfully connected to the PostgreSQL database!")
+    conn.close()
+    
+except Exception as e:
+    print(f"non potete andare in bagno, troppi caffè: {e}")
