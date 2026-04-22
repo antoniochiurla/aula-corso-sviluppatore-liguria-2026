0540485190fced3fb43d56d23aa1ff40846c16f5
antoniochiurla <antoniochiurla@gmail.com>
2026-04-22 08:36:40 +0000

    added mytamasy

---
+++ mytamasy/tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path("", views.index, name="index"),
+    path('add/<str:tipo>/', views.add_task, name='add_task'), # Aggiunta
+    path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'), # Cambio stato
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'), # Rimozione
+]
---
+++ mytamasy/tasks/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+
+from .models import Task, BugTask, FeatureTask
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    # Recuperiamo tutti i task dal database
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/index.html', {'tasks': tasks})
+
+@login_required
+def add_task(request, tipo):
+    if request.method == "POST":
+        titolo = request.POST.get('titolo')
+        desc = request.POST.get('descrizione')
+
+        if tipo == 'bug':
+            BugTask.objects.create(created_by=request.user,title=titolo, description=desc, severity='ME')
+        elif tipo == 'feature':
+            FeatureTask.objects.create(created_by=request.user,title=titolo, description=desc, priority='2')
+        else:
+            Task.objects.create(created_by=request.user,title=titolo, description=desc)
+
+        return redirect('index')
+    return render(request, 'tasks/add_form.html', {'tipo': tipo})
+
+
+def toggle_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.status = 'CL' if task.status == 'AP' else 'AP'
+    task.save()
+    return redirect('index')
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('index')
