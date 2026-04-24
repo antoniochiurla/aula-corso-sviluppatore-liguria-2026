8aec037e635c78b01463d2f4d3ef24812614defb
antoniochiurla <antoniochiurla@gmail.com>
2026-04-24 08:03:45 +0000

    distinguish task types

--- mytamasy/tasks/urls.py
+++ mytamasy/tasks/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path("", views.index, name="index"),
     path('logout/', views.logout_view, name='logout'),
     path('add/<str:tipo>/', views.add_task, name='add_task'), # Aggiunta
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'), # Modifica
     path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'), # Cambio stato
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'), # Rimozione
 ]
--- mytamasy/tasks/views.py
+++ mytamasy/tasks/views.py
@@ -9,7 +9,51 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     # Recuperiamo tutti i task dal database
     tasks = Task.objects.all().order_by('-created_at')
-    return render(request, 'tasks/index.html', {'tasks': tasks})
+    colors = {
+        'T': 'primary',
+        'B': 'danger',
+        'F': 'warning'}
+    icons = {
+        'T': '...',
+        'B': '🐞',
+        'F': '⭐'
+    }
+    context = {
+        'tasks': tasks,
+        'colors': colors,
+        'icons': icons}
+    return render(request, 'tasks/index.html', context)
+
+@login_required
+def edit_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    bug_task = None
+    feature_task = None
+    if task.type == 'B':
+        bug_task = BugTask.objects.get(pk=task_id)
+    if task.type == 'F':
+        feature_task = FeatureTask.objects.get(pk=task_id)
+    if request.method == "POST":
+        bug_task.title = request.POST.get('titolo')
+        bug_task.description = request.POST.get('descrizione')
+        if task.type == 'B':
+            bug_task.severity = request.POST.get('severity')
+            bug_task.save()
+        elif task.type == 'F':
+            feature_task.priority = request.POST.get('priority')
+            feature_task.save()
+        else:
+            task.save()
+
+        return redirect('index')
+    context = {
+        'tipo': task.type,
+        'task': task,
+        'bug': bug_task,
+        'feature': feature_task,
+        'types': {'T': 'Task', 'B': "Bug", 'F': 'Feature'}
+        }
+    return render(request, 'tasks/add_form.html', context)
 
 @login_required
 def add_task(request, tipo):
@@ -28,6 +72,7 @@ def add_task(request, tipo):
     return render(request, 'tasks/add_form.html', {'tipo': tipo})
 
 
+@login_required
 def toggle_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.status = 'CL' if task.status == 'AP' else 'AP'
@@ -35,10 +80,12 @@ def toggle_task(request, task_id):
     return redirect('index')
 
 
+@login_required
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('index')
 
+@login_required
 def logout_view(request):
     logout(request)
